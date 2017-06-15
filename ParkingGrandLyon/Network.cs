cd65014953d5fd40b83e05f9876ec628b7faec24
Sylvain Laure <slaure.extern@groupeseb.com>
2017-06-15 16:48:02 +0200

    Attention, ça crashe !

--- ParkingGrandLyon/Network.cs
+++ ParkingGrandLyon/Network.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace ParkingGrandLyon {
-    public class Network {
-        HttpClient client = new HttpClient();
-        public Network() {
-            GetLocations();
-        }
+public class Network
+{
+	HttpClient client = new HttpClient();
 
-        public async Task<String> GetParkings(int limit) {
-			var response = await client.GetStringAsync("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvoparking/all.json?maxfeatures="+limit);
-            Console.WriteLine(response);
-			return response;
-		}
-
-        public async void GetLocations() {
-			var response = await client.GetStringAsync("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvoparking/the_geom.json");
-			Console.WriteLine(response);
+	static string authorization = "Basic cGF1bGluZS5tb250Y2hhdUBnbWFpbC5jb206RXBpdGVjaDQyLjIwMDc=";
+	static string parkingsUrl = "https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=30&request=GetFeature&typename=pvo_patrimoine_voirie.pvoparkingtr&SRSNAME=urn:ogc:def:crs:EPSG::4171";
+	public Network()
+	{
+		//GetLocations();
+	}
 
 
-		}
+	public async Task<string> GetParkings(int limit)
+	{
+		client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authorization);
+		var response = await client.GetStringAsync(parkingsUrl);
+		return response;
+	}
+
+
 
     }
 }
