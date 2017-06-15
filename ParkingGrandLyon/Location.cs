1f88e500c366a5bdd9e934606eadda9c831b1e16
Hugo PATURAL <hpatural69@gmail.com>
2017-06-15 15:58:37 +0200

    get locations

--- ParkingGrandLyon/Location.cs
+++ ParkingGrandLyon/Location.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace ParkingGrandLyon
 {
 	public class Location
 	{
-		float latitutde { get; set; }
-		float longitude { get; set; }
+		double latitude { get; set; }
+		double longitude { get; set; }
 
 		public Location()
 		{
+		}
+
+		public static Location createFromJson(String json)
+		{
+            Location locationTest = new Location { latitude = 49.1, longitude = 29.1 };
+            Console.Out.WriteLine("parking cree : " + locationTest.latitude);
+			var myJson = JsonConvert.SerializeObject(locationTest);
+			Console.Out.WriteLine("serialized object : " + myJson);
+			Location deserializedLocation = JsonConvert.DeserializeObject<Location>(myJson);
+			Console.Out.WriteLine("deserialized location :  " + deserializedLocation.latitude);
+			return deserializedLocation;
+			//Parking p = JsonConvert.DeserializeObject<Parking>(json);
+			//Console.Out.WriteLine("json object : "  + json);
+			//Console.Out.WriteLine("deserialized object : " + p.nom);
+			//return p;
 		}
 	}
 }
--- ParkingGrandLyon/Network.cs
+++ ParkingGrandLyon/Network.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace ParkingGrandLyon {
     public class Network {
         HttpClient client = new HttpClient();
+
+        static string authorization = "Basic cGF1bGluZS5tb250Y2hhdUBnbWFpbC5jb206RXBpdGVjaDQyLjIwMDc=";
         public Network() {
             GetLocations();
         }
 
         public async void GetParkings(int limit) {
+
+			client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authorization);
+
 			var response = await client.GetStringAsync("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvoparking/all.json?maxfeatures="+limit);
             Console.WriteLine(response);
 		}
 
         public async void GetLocations() {
-			var response = await client.GetStringAsync("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvoparking/the_geom.json");
-			Console.WriteLine(response);
 
+			client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authorization);
+
+			var response = await client.GetStringAsync("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=30&request=GetFeature&typename=pvo_patrimoine_voirie.pvoparkingtr&SRSNAME=urn:ogc:def:crs:EPSG::4171");
+			Console.WriteLine(response);
+            var myJson = JObject.Parse(response);
+
+            Console.WriteLine(myJson["features"]);
+            //Location location = Location.createFromJson()
+           
 
 		}
 
