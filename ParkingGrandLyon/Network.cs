520cbc76186303e5f9423ff049a8039ad9d22666
Hugo PATURAL <hpatural69@gmail.com>
2017-06-15 16:25:31 +0200

    update api

--- ParkingGrandLyon/Network.cs
+++ ParkingGrandLyon/Network.cs
@@ -2,33 +2,31 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace ParkingGrandLyon {
     public class Network {
         HttpClient client = new HttpClient();
 
         static string authorization = "Basic cGF1bGluZS5tb250Y2hhdUBnbWFpbC5jb206RXBpdGVjaDQyLjIwMDc=";
+        static string parkingsUrl = "https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=30&request=GetFeature&typename=pvo_patrimoine_voirie.pvoparkingtr&SRSNAME=urn:ogc:def:crs:EPSG::4171";
         public Network() {
-            GetLocations();
+            //GetLocations();
         }
 
-        public async void GetParkings(int limit) {
-
+        public async Task<string> GetParkings(int limit) {
 			client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authorization);
-
-			var response = await client.GetStringAsync("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvoparking/all.json?maxfeatures="+limit);
-            Console.WriteLine(response);
+            var response = await client.GetStringAsync(parkingsUrl);
+            return response;
 		}
 
-        public async void GetLocations() {
+        public async Task<string> GetLocations() {
 
 			client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(authorization);
 
-			var response = await client.GetStringAsync("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=30&request=GetFeature&typename=pvo_patrimoine_voirie.pvoparkingtr&SRSNAME=urn:ogc:def:crs:EPSG::4171");
-			Console.WriteLine(response);
+            var response = await client.GetStringAsync(parkingsUrl);
             var myJson = JObject.Parse(response);
-
-            Console.WriteLine(myJson["features"]);
+            return null;
             //Location location = Location.createFromJson()
            
 
