02581b46219208682b742652b881d9749f515575
Hugo PATURAL <hpatural69@gmail.com>
2017-06-16 17:55:43 +0200

    change api key

--- ParkingGrandLyon/Network.cs
+++ ParkingGrandLyon/Network.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -12,7 +12,7 @@ namespace ParkingGrandLyon
 
 		static string authorization = "Basic cGF1bGluZS5tb250Y2hhdUBnbWFpbC5jb206RXBpdGVjaDQyLjIwMDc=";
 		static string parkingsUrl = "https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=30&request=GetFeature&typename=pvo_patrimoine_voirie.pvoparkingtr&SRSNAME=urn:ogc:def:crs:EPSG::4326&bbox={0},{1},{2},{3}";
-		static string directionsUrl = "https://maps.googleapis.com/maps/api/directions/json?origin={0},{1}&destination={2},{3}&mode=DRIVING&key=AIzaSyDTLYMpxG8TtIRP_e9_4EnjGvCFrZ6v2dg";
+		static string directionsUrl = "https://maps.googleapis.com/maps/api/directions/json?origin={0},{1}&destination={2},{3}&mode=DRIVING&key=AIzaSyDt-2m8MNOGOVI5yRcBhEDLYj4YZpUMje0";
 
 		public async Task<string> GetParkings(int limit, Location currentLocation)
 		{
