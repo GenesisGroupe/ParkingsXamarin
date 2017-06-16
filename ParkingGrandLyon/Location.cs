71fbacc3c4df1d8a0f101204eb7dd376f7acfea6
Hugo PATURAL <hpatural69@gmail.com>
2017-06-16 09:31:23 +0200

    get location

--- ParkingGrandLyon/Location.cs
+++ ParkingGrandLyon/Location.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
 namespace ParkingGrandLyon
 {
 	public class Location
@@ -11,5 +15,32 @@ namespace ParkingGrandLyon
 			this.latitude = lat;
 			this.longitude = longitude;
 		}
+
+		public Location()
+		{
+			
+		}
+
+        public async Task<Location> getCurrentPosition() {
+            var locator = CrossGeolocator.Current;
+			locator.DesiredAccuracy = 100; //100 is new default
+
+            if (locator.AllowsBackgroundUpdates) {
+				var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+				Console.WriteLine("Position Status: {0}", position.Timestamp);
+
+				Console.WriteLine("Position Latitude: {0}", position.Latitude);
+
+				Console.WriteLine("Position Longitude: {0}", position.Longitude);
+
+				Location location = new Location((float)position.Longitude, (float)position.Latitude);
+
+				return location;
+            } 
+
+            return null;
+        }
+
 	}
 }
